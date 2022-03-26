c0f56ceb6c4751872e584a3a865f628f3de2d124
Leesin0222 <dragon02229@naver.com>
2022-03-26 20:54:06 +0900

    ⚡ :: background = null & menu getitem(1) Enabled false 설정해주기

--- app/src/main/java/com/yongjincompany/onpost/MainActivity.kt
+++ app/src/main/java/com/yongjincompany/onpost/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.yongjincompany.onpost
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yongjincompany.onpost.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.menu.getItem(1).isEnabled = false
+
+
     }
 }
