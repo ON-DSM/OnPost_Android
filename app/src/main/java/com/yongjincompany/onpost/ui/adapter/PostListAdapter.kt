ad7c9f5d3220bb88e7973a227806c06f5a3e78a3
Leesin0222 <dragon02229@naver.com>
2022-07-02 23:00:58 +0900

    ⚡ :: 게시물정렬 api 에서 image 불러옴

--- app/src/main/java/com/yongjincompany/onpost/ui/adapter/PostListAdapter.kt
+++ app/src/main/java/com/yongjincompany/onpost/ui/adapter/PostListAdapter.kt
@@ -1,19 +1,24 @@
 package com.yongjincompany.onpost.ui.adapter
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.yongjincompany.onpost.R
 import com.yongjincompany.onpost.databinding.ItemPostBinding
 import com.yongjincompany.onpost.remote.response.SortPostResponse
+import kotlin.coroutines.coroutineContext
 
 class PostListAdapter :
     RecyclerView.Adapter<PostListAdapter.PostDataViewHolder>() {
     inner class PostDataViewHolder(val itemPostBinding: ItemPostBinding) :
         RecyclerView.ViewHolder(itemPostBinding.root)
 
+    private lateinit var context:Context
     private var myList = emptyList<SortPostResponse>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PostDataViewHolder(
@@ -28,16 +33,20 @@ class PostListAdapter :
     override fun getItemCount(): Int = myList.size
 
     override fun onBindViewHolder(holder: PostDataViewHolder, position: Int) {
+        context = holder.itemPostBinding.userProfileImage.context
         holder.itemPostBinding.title.text = myList[position].title
         holder.itemPostBinding.name.text = myList[position].writer.name
         holder.itemPostBinding.likeText.text = myList[position].like.toString()
         holder.itemPostBinding.date.text = myList[position].createAt
         holder.itemPostBinding.tag.text = myList[position].tags
+        Glide.with(context).load(myList[position].profileImage).into(holder.itemPostBinding.ivPost)
+        Glide.with(context).load(myList[position].writer.profile).into(holder.itemPostBinding.userProfileImage)
     }
 
     @SuppressLint("NotifyDataSetChanged")
     fun setData(newList: List<SortPostResponse>) {
         myList = newList
         notifyDataSetChanged()
+
     }
 }
