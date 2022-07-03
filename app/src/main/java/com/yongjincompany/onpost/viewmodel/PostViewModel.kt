1b41e6ec8de0943bfa2026a46d11b198ba41750a
Leesin0222 <dragon02229@naver.com>
2022-07-03 22:40:16 +0900

    ⚡ :: top3 viewmodel 작성

--- app/src/main/java/com/yongjincompany/onpost/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/yongjincompany/onpost/viewmodel/PostViewModel.kt
@@ -4,17 +4,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yongjincompany.onpost.remote.response.SortPostResponse
+import com.yongjincompany.onpost.remote.response.TopThreePostResponse
 import com.yongjincompany.onpost.repository.PostRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class PostViewModel(private val postRepository: PostRepository) : ViewModel() {
-    var modo: MutableLiveData<Response<List<SortPostResponse>>> = MutableLiveData()
+    var sortPostLiveData: MutableLiveData<Response<List<SortPostResponse>>> = MutableLiveData()
+    var topThreePostLiveData: MutableLiveData<Response<List<TopThreePostResponse>>> = MutableLiveData()
 
     fun getPost(sort: String, page: Int) {
         viewModelScope.launch {
             val response = postRepository.getPost(sort, page)
-            modo.value = response
+            sortPostLiveData.value = response
+        }
+    }
+
+    fun getTopThreePost(sort: String) {
+        viewModelScope.launch {
+            val response = postRepository.getTopThreePost(sort)
+            topThreePostLiveData.value = response
         }
     }
 }
