2e9cd48d32dea5f2304fe602f90ab3f3fa682d09
Leesin0222 <dragon02229@naver.com>
2022-07-03 22:39:04 +0900

    ⚡ :: top3 API 작성

--- app/src/main/java/com/yongjincompany/onpost/remote/api/PostApi.kt
+++ app/src/main/java/com/yongjincompany/onpost/remote/api/PostApi.kt
@@ -1,6 +1,7 @@
 package com.yongjincompany.onpost.remote.api
 
 import com.yongjincompany.onpost.remote.response.SortPostResponse
+import com.yongjincompany.onpost.remote.response.TopThreePostResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,4 +12,9 @@ interface PostApi {
         @Query("sort") sort: String,
         @Query("page") page: Int,
     ): Response<List<SortPostResponse>>
+
+    @GET("post/top3")
+    suspend fun fetchTopPost(
+        @Query("sort") sort: String
+    ): Response<List<TopThreePostResponse>>
 }
