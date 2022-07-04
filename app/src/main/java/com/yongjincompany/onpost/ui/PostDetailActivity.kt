41a356f785be4ffa6f8b42d43d4a5607721e6706
Leesin0222 <dragon02229@naver.com>
2022-07-04 12:07:01 +0900

    ⚡ :: 게시물 상세보기 Activity에 구현

---
+++ app/src/main/java/com/yongjincompany/onpost/ui/PostDetailActivity.kt
@@ -0,0 +1,53 @@
+package com.yongjincompany.onpost.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.yongjincompany.onpost.databinding.ActivityMainBinding
+import com.yongjincompany.onpost.databinding.ActivityPostDetailBinding
+import com.yongjincompany.onpost.repository.PostRepository
+import com.yongjincompany.onpost.viewmodel.PostViewModel
+import com.yongjincompany.onpost.viewmodel.PostViewModelFactory
+
+class PostDetailActivity : AppCompatActivity() {
+
+    private lateinit var vm: PostViewModel
+    private lateinit var binding: ActivityPostDetailBinding
+    var data: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPostDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val repository = PostRepository()
+        val viewModelFactory = PostViewModelFactory(repository)
+        vm = ViewModelProvider(this, viewModelFactory).get(PostViewModel::class.java)
+
+        data = intent.getIntExtra("postid", data)
+
+        vm.getShowPost(id = data)
+        vm.showPostLiveData.observe(this, Observer { response ->
+            if (response.isSuccessful) {
+                response.body().let {
+                    if (it != null) {
+                        binding.content.text = it.content
+                        binding.tag.text = it.tags
+                        binding.title.text = it.title
+                        binding.date.text = it.createAt
+                        binding.writer.text = it.writer.name
+                        binding.like.text = it.like.toString()
+                    }
+                }
+            } else {
+                Log.d("error", response.errorBody().toString())
+                Toast.makeText(applicationContext,
+                    response.errorBody().toString(),
+                    Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
