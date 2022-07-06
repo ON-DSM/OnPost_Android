d4b77b7f6383d825050ba8ffaa8a0b1de1620c0d
Leesin0222 <dragon02229@naver.com>
2022-07-06 11:37:12 +0900

    ⚡ :: Manifest에 Activity들들 추가 및 UserInfoActivity 생성

---
+++ app/src/main/java/com/yongjincompany/onpost/ui/member/UserInfoActivity.kt
@@ -0,0 +1,14 @@
+package com.yongjincompany.onpost.ui.member
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yongjincompany.onpost.R
+
+class UserInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+
+
+    }
+}
