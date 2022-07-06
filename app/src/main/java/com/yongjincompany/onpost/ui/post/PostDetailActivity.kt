79c1dac67a58e0123e440d8b79776734dc1a0639
Leesin0222 <dragon02229@naver.com>
2022-07-07 01:34:16 +0900

    ⚡ :: 댓글 보여주기 구현

--- app/src/main/java/com/yongjincompany/onpost/ui/post/PostDetailActivity.kt
+++ app/src/main/java/com/yongjincompany/onpost/ui/post/PostDetailActivity.kt
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.yongjincompany.onpost.databinding.ActivityMainBinding
 import com.yongjincompany.onpost.databinding.ActivityPostDetailBinding
 import com.yongjincompany.onpost.repository.PostRepository
+import com.yongjincompany.onpost.ui.post.adapter.CommentListAdapter
+import com.yongjincompany.onpost.ui.post.adapter.PostListAdapter
 import com.yongjincompany.onpost.viewmodel.post.PostViewModel
 import com.yongjincompany.onpost.viewmodel.post.PostViewModelFactory
 
@@ -17,11 +22,17 @@ class PostDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostDetailBinding
     var data: Int = 0
 
+    private val myAdapter by lazy {
+        CommentListAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityPostDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupRecylerview()
 
         val repository = PostRepository()
         val viewModelFactory = PostViewModelFactory(repository)
@@ -33,14 +44,21 @@ class PostDetailActivity : AppCompatActivity() {
         vm.showPostLiveData.observe(this, Observer { response ->
             if (response.isSuccessful) {
                 response.body().let {
-                    if (it != null) {
-                        binding.content.text = it.content
+
+                        binding.content.text = it!!.content
                         binding.tag.text = it.tags
                         binding.title.text = it.title
                         binding.date.text = it.createAt
                         binding.writer.text = it.writer.name
                         binding.like.text = it.like.toString()
+                        Glide.with(applicationContext).load(it.writer.profile).into(binding.ivWriter)
+                        binding.writerName.text = it.writer.name
+                        binding.writerContent.text = it.writer.introduce
+                        myAdapter.setData(it.comments)
+                    if(it.tags == null) {
+                        binding.tag.text = "태그 정보가 없습니다"
                     }
+
                 }
             } else {
                 Toast.makeText(applicationContext,
@@ -48,5 +66,13 @@ class PostDetailActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT).show()
             }
         })
+
+
+    }
+
+    private fun setupRecylerview() {
+        val recylerView = binding.commentList
+        recylerView.adapter = myAdapter
+        recylerView.layoutManager = LinearLayoutManager(this)
     }
 }
